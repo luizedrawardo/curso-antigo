8448a3994731e291b29a1cc2b2fe9163e3e36b98
luizedrawardo <eduardo007.costa@gmail.com>
2022-07-21 03:16:39 -0300

    a

--- estudo de position e lampada/lampada.js
+++ estudo de position e lampada/lampada.js
@@ -3,11 +3,11 @@ const lampada=window.document.getElementById('lampada')
 
 function mudarlampada(valor){
     if(valor==1){
-        lampada.src='dados do curso/_imagens/lampada-acesa.jpg'  
+        lampada.src='lampada-acesa.jpg'  
     }else if(valor==2){
-        lampada.src='dados do curso/_imagens/lampada-apagada.jpg' 
+        lampada.src='lampada-apagada.jpg' 
     }else if(valor==3){
-        lampada.src='dados do curso/_imagens/lampada-quebrada.jpg'
+        lampada.src='lampada-quebrada.jpg'
         quebrada=true
     }
     // nao consegui fazer a função do quebrado e tals tentar depois
